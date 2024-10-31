7c98f2f404e8a9576b52c4fa081eb533d273d0f6
kylbol <119703769+kylbol@users.noreply.github.com>
2024-10-31 10:04:27 -0400

    Update Castle.py

--- GameElements/Castle.py
+++ GameElements/Castle.py
@@ -34,55 +34,6 @@ class Castle:
         self.shield = 50
         self.gold = 50
 
-
-
-    def getHealth(self):
-        return self.health
-    
-    def setHealth(self,health):
-        if (health > self.CONST_MAXHEALTH):
-            self.health = self.CONST_MAXHEALTH
-        else:
-            self.health = health
-        return self.health
-    
-    def removeHealth(self,remove):
-        self.health -= remove
-        return self.health
-    
-    def addHealth(self,add):
-        newHealth = add + self.health
-        if (newHealth > self.CONST_MAXHEALTH):
-            self.health = self.CONST_MAXHEALTH
-        else:
-            self.health = newHealth
-        return self.health
-
-    def getShield(self):
-        return self.health
-    
-    def setShield(self,shield):
-        self.shield = shield
-    
-    def removeShield(self,remove):
-        self.shield -= remove
-        return self.shield
-    
-    def getGold(self):
-        return self.gold
-    
-    def setGold(self,gold):
-        self.gold = gold
-    
-    def removeGold(self,remove):
-        if (self.gold >= remove):
-            self.gold -= remove
-            return self.gold
-        return -1
-    
-    def addGold(self,add):
-        self.gold += add
-        return self.gold
 #draw castle
     def draw(self, win):
         self.image = self.bg_img2
