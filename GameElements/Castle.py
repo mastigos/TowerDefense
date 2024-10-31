74da6bcf9c6da58f2f99612f73592955a235b4f0
ClaireBear1214 <90657428+ClaireBear1214@users.noreply.github.com>
2024-10-31 10:03:54 -0400

    Update Castle.py
    I did it!

--- GameElements/Castle.py
+++ GameElements/Castle.py
@@ -1,4 +1,7 @@
 #by Kyle
+import pygame
+from pygame.examples.cursors import image
+
 
 import GameElement
 
@@ -6,18 +9,32 @@ import GameElement
 # Contains the fields health, shield, and gold
 # Once health reaches 0 (or below), the player looses
 # Gold is the amount of funds the player can spend on towers
-class Castle(GameElement):
+class Castle:
     CONST_MAXHEALTH = 100
     health = 0
     shield = 0
     gold = 0
 
-    def __init__(self):
+    def __init__(self, bg_img2, x, y, scale):
         # body of the constructor
-        GameElement.setSpite("./sprites/Castle.png")
         self.health = 100
+        self.maxHealth = self.health
+        self.sprite = bg_img2
+        #getting width/height of the og castle img
+        width = bg_img2.get_width()
+        height = bg_img2.get_height()
+
+        self.bg_img2 = pygame.transform.scale(bg_img2, (int (width*scale), int (height*scale)))
+
+        #need a rectangle of this castle image for hit box, collision, and positioning
+        self.rect = self.bg_img2.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
         self.shield = 50
         self.gold = 50
+
+
 
     def getHealth(self):
         return self.health
@@ -66,3 +83,9 @@ class Castle(GameElement):
     def addGold(self,add):
         self.gold += add
         return self.gold
+#draw castle
+    def draw(self, win):
+        self.image = self.bg_img2
+        win.blit(self.image, self.rect)
+
+
